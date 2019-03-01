a1ddf9f4b2a55d4810c4abfec9693e2264672ecd
geermc4 <geermc4@gmail.com>
2019-03-01 10:35:05 -0800

    fix (@$#*& :shit:

---
+++ lib/splash_payments/resource/mixin.rb
@@ -0,0 +1,9 @@
+module SplashPayments
+  module Resource
+    module Mixin
+      require_relative 'mixin/create_disabled'
+      require_relative 'mixin/update_disabled'
+      require_relative 'mixin/delete_disabled'
+    end
+  end
+end
